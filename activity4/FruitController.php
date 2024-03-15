34b39bfa6c68c87ce9f4af09bf295bcd6f593565
Tan <tan.a25@student.passerellesnumeriques.org>
2024-03-15 08:23:39 +0700

    first commit

---
+++ activity4/FruitController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Fruit;
+
+
+class FruitController extends Controller
+{
+    public function getFruits()
+    {
+        return "I have no fruits !";
+    }
+}
